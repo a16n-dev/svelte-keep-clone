61cfd65845c85eb9ccac2dbcfa84fb89f2380eb4
alexn400 <alexn400@gmail.com>
2022-03-15 17:24:25 +1300

    Add sass

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Post from '../components/Post.svelte';
+</script>
+
+<Post content="3" title="post title" />
